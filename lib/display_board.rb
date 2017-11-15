6b106f83199f20fc13fe69fbb98a1f1e106eed9c
Yufeng Liu Reach <yufeng8@vt.edu>
2017-11-15 19:11:46 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,7 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board()
   board = ["   |   |   ", "-----------", "   |   |   ", "-----------", "   |   |   "]
+  return board
 end
 
 display_board()
